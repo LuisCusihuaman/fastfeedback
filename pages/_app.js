86e2ccbd6ba485aab78274062c12eed847915539
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-02-13 12:49:41 -0300

    feat: global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,28 @@
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { AuthProvider } from '@/lib/auth';
 import customTheme from '@/styles/theme';
+import { Global, css } from '@emotion/react';
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      html {
+        min-width: 360px;
+        scroll-behavior: smooth;
+      }
+      #__next {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+      }
+    `}
+  />
+);
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={customTheme} resetCSS>
+      <GlobalStyle />
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
